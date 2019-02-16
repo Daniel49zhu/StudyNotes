7ebd4392743541f94f8efd48177c5894d51f9de0
Daniel49zhu <zhujiacheng94@163.com>
2019-02-16 22:16:16 +0800

    demo-camera

---
+++ Electron/demo-camera/main.js
@@ -0,0 +1,26 @@
+const {app,BrowserWindow} = require('electron');
+
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed',()=> {
+    if(process.platform!='darwin') {
+        app.quit();
+    }
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready',()=> {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width:800,height:600});
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow.on('closed',()=>{
+       mainWindow = null;
+    });
+});
+
