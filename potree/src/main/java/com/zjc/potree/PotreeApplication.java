ff781789d973a8afb1902a4e61d77663b569b0bb
Daniel49zhu <zhujiacheng94@163.com>
2019-02-22 00:15:22 +0800

    potree

---
+++ potree/src/main/java/com/zjc/potree/PotreeApplication.java
@@ -0,0 +1,13 @@
+package com.zjc.potree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotreeApplication.class, args);
+	}
+
+}
