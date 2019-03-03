6816e1be29d89d98af4002d1e51ef53498de027a
Daniel49zhu <zhujiacheng94@163.com>
2019-03-03 22:42:33 +0800

    React

--- React/dissecting-react-and-redux/chapter-04/todo/src/index.js
+++ React/dissecting-react-and-redux/chapter-04/todo/src/index.js
@@ -6,8 +6,8 @@ import TodoApp from './TodoApp';
 import store from './Store.js';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <TodoApp />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
 );
--- React/dissecting-react-and-redux/chapter-04/todo/src/todos/actionTypes.js
+++ React/dissecting-react-and-redux/chapter-04/todo/src/todos/actionTypes.js
@@ -1,4 +1,4 @@
-//为了防止命令冲突所以加上了`TODO/`前缀
 export const ADD_TODO = 'TODO/ADD';
 export const TOGGLE_TODO = 'TODO/TOGGLE';
 export const REMOVE_TODO = 'TODO/REMOVE';
+
