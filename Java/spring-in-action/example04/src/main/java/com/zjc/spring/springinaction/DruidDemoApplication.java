d8c4c320fa6da605a9b5faa83dba48a0e7c094a8
Daniel49zhu <zhujiacheng94@163.com>
2019-04-07 22:50:21 +0800

    example04

---
+++ Java/spring-in-action/example04/src/main/java/com/zjc/spring/springinaction/DruidDemoApplication.java
@@ -0,0 +1,37 @@
+package com.zjc.spring.springinaction;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class DruidDemoApplication {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDemoApplication.class, args);
+    }
+
+    @RequestMapping(path = "/run")
+    public void run(HttpServletResponse response) throws Exception {
+        PrintWriter writer = response.getWriter();
+
+        writer.println(dataSource.toString());
+        jdbcTemplate.queryForList("SELECT * FROM FOO").forEach(list -> writer.println(list));
+
+        writer.close();
+    }
+}
