fd73e00aa970a8b36fc02485823e9691aa55cf98
Daniel49zhu <zhujiacheng94@163.com>
2019-04-07 16:27:29 +0800

    example03

--- Java/spring-in-action/example03/src/main/java/com/zjc/spring/MultiDataSourceApplication.java
+++ Java/spring-in-action/example03/src/main/java/com/zjc/spring/MultiDataSourceApplication.java
@@ -1,6 +1,6 @@
 package com.zjc.spring;
 
-import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,39 +8,76 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class})
 @RestController
+@Slf4j
 public class MultiDataSourceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MultiDataSourceApplication.class,args);
     }
 
+    @RequestMapping(path="/run")
+    public void run(HttpServletResponse response) throws Exception {
+        PrintWriter writer = response.getWriter();
+        Connection fooConnection = this.fooDataSource().getConnection();
+        writer.println(fooConnection.toString());
+        fooConnection.close();
+
+        Connection barConnection = this.barDataSource().getConnection();
+        writer.println(barConnection.toString());
+        barConnection.close();
+    }
+
+
     @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource.first")
-    public DataSourceProperties firstDataSourceProperties() {
+    @ConfigurationProperties(prefix = "spring.datasource.foo")
+    public DataSourceProperties fooDataSourceProperties() {
         return new DataSourceProperties();
     }
 
     @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource.first.configuration")
-    public HikariDataSource firstDataSource() {
-        return firstDataSourceProperties().initializeDataSourceBuilder()
-                .type(HikariDataSource.class).build();
+    public DataSource fooDataSource() {
+        DataSourceProperties dataSourceProperties = fooDataSourceProperties();
+        return dataSourceProperties.initializeDataSourceBuilder().build();
     }
 
     @Bean
-    @ConfigurationProperties("app.datasource.second")
-    public DataSource secondDataSource() {
-        return DataSourceBuilder.create().type(DataSource.class).build();
+    @Resource
+    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.bar")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties dataSourceProperties = barDataSourceProperties();
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        return new DataSourceTransactionManager(barDataSource);
     }
 
 }
