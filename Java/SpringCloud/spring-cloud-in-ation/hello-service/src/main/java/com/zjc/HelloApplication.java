cec1955bfbdc26e0587a06b84e3fd5f07f609030
Daniel49zhu <zhujiacheng94@163.com>
2019-06-27 23:38:40 +0800

    Eureka

---
+++ Java/SpringCloud/spring-cloud-in-ation/hello-service/src/main/java/com/zjc/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.zjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+//@EnableHystrix
+// EnableDiscoveryClient激活Eureka中的DiscoveryClient实现
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
