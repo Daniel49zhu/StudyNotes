5dedf0668b840be079b6b21d838c97c546848557
Daniel49zhu <zhujiacheng94@163.com>
2019-07-21 23:15:56 +0800

    golang

---
+++ LeetCode/go/src/0001.two-sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//a := []int{2,3,4,5}
+	fmt.Println(twoSum(nil, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+
+	for i, b := range nums {
+		if j, ok := index[target-b]; ok {
+			return []int{j, i}
+		}
+		index[b] = i
+	}
+	return nil
+}
