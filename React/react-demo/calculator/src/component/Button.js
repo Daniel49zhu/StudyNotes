9ef4e4fb794c13dac7880b96320b36ff6366e176
Daniel49zhu <zhujiacheng94@163.com>
2019-08-14 23:52:33 +0800

    react-demo

---
+++ React/react-demo/calculator/src/component/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Button.css'
+import PropTypes from "prop-types";
+
+export default class Button  extends React.Component {
+
+    handleClick = () =>{
+        debugger
+        console.log(this.props.name+" Button")
+        this.props.clickHandler(this.props.name);
+    }
+
+    render() {
+        const className = [
+            "component-button",
+            this.props.orange ? "orange" : "",
+            this.props.wide ? "wide" : "",
+          ];
+
+        return (<div className={className.join(" ").trim()}>
+            <button  onClick={this.handleClick}>{this.props.name}</button>
+        </div>);
+    }
+}
