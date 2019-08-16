c11b1036de527bbb482ef0663c924e648a89d6d3
Daniel49zhu <zhujiacheng94@163.com>
2019-08-16 17:36:25 +0800

    react-demo

---
+++ React/react-demo/gtihub-battle/src/component/Popular/RepoGrid.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default class RepoGrid extends React.Component {
+    render = ()=> {
+        return (
+            <ul className = "popular-list">
+                {this.props.repos.map((repo,index)=>{
+                              return (
+                                <li
+                                  key={repo.name}
+                                  //className={`popular-item ${index % 2 ? 'to-right' : 'to-left'}`}
+                                  className="popular-item"
+                                >
+                                  <div className="popular-rank">#{index + 1}</div>
+                                  <ul className="space-list-items">
+                                    <li>
+                                      <img
+                                        className="avatar"
+                                        src={repo.owner.avatar_url}
+                                        alt={`Avatar for ${repo.owner.login}`}
+                                      />
+                                    </li>
+                    
+                                    <li>
+                                      <a href={repo.html_url} target="_blank">
+                                        {repo.name}
+                                      </a>
+                                    </li>
+                                    <li>@{repo.owner.login}</li>
+                                    <li>{repo.stargazers_count} stars</li>
+                                  </ul>
+                                </li>
+                              );
+                })}
+            </ul>
+        )
+    }
+}
