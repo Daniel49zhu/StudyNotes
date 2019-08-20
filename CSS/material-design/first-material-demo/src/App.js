e22fef99f42ae93e904fab2fb1738c5d537a3d30
Daniel49zhu <zhujiacheng94@163.com>
2019-08-20 23:13:22 +0800

    react-demo

---
+++ CSS/material-design/first-material-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box'
+import SignInSide from './SignInSide';
+
+function App() {
+  return (
+      <SignInSide />
+  );
+}
+
+export default App;
