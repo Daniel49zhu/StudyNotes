959ecf3d14db62d273519f491ae004696b58afc1
Daniel49zhu <zhujiacheng94@163.com>
2019-09-08 22:54:54 +0800

    design-pattern

---
+++ Java/design-pattern/proxy/src/main/java/com/zjc/IvoryTower.java
@@ -0,0 +1,8 @@
+package com.zjc;
+
+public class IvoryTower implements WizardTower {
+    @Override
+    public void enter(Wizard wizard) {
+        System.out.printf("%s enters the tower.\n", wizard);
+    }
+}
---
+++ Java/design-pattern/proxy/src/main/java/com/zjc/WizardTower.java
@@ -0,0 +1,5 @@
+package com.zjc;
+
+public interface WizardTower {
+    void enter(Wizard wizard);
+}
