18c18bc30a045469952e2027e39837bdfa0c44de
Daniel49zhu <zhujiacheng94@163.com>
2019-09-10 22:13:10 +0800

    spring-boot-examples

---
+++ Java/spring-boot-examples/spring-boot-hello/src/main/java/com/zjc/springboothello/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.zjc.springboothello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot 2.0!";
+    }
+}
